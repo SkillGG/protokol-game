9dcbe9987401465e84d2c08e35dcc59e400929ff
Marcin Majewski <github@skilletss.cba.pl>
2019-02-03 21:24:11 +0100

    Prepared for future menu update

--- Game.js
+++ Game.js
@@ -1,9 +1,24 @@
 let Game = {};
 let justType = {type: true};
 
-Game.start = function(GameData){
+Game.start = async function(GameData){
 	// Start Game
 	if(!GameData)
-		throw "GData";
+		throw "No GameData";
 	this.data = GameData;
+	await new Promise(async (rs,rj)=>{
+		if(/none/.test(this.data.language)){
+
+			Screen.addLines(
+				Lang.getSpan(`menu1`)
+				);
+
+			await Screen.applyToAll(true,true,true,justType);
+
+
+		}
+		else
+			rs();
+	})
+	.catch(console.error);
 }
